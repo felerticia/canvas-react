296660b0bb4b90875c861d545f00eac8aa95438f
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-03-06 11:25:26 +0100

    Hook!

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import Canvas from "./Canvas/Canvas";
 
 function App() {
   const draw = (context,count) => {
-
-
     context.clearRect(0, 0, context.canvas.width, context.canvas.height)
     context.fillStyle = 'grey'
     const d = count % 800
     context.fillRect(10 +d , 10  , 100 , 100)
-
   }
 
+  const draw2 = (context,count) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+    context.fillStyle = 'grey'
+    const d = count % 100
+    context.fillRect(10 +d , 10  , 10 , 10)
+  }
 
-  return <Canvas draw={draw} width="800" height="800" style={{border:'1px solid black'}}/>
+  return <div>
+    <Canvas draw={draw} width="800" height="800" style={{border:'1px solid black'}}/>
+    <Canvas draw={draw2} width="100" height="100" style={{border:'1px solid black'}}/>
+  </div>
 }
 
 export default App;
--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -1,31 +1,13 @@
 
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
+import useCanvas from './useCanvas'
 
-const Canvas = props => {
+const Canvas = props => {  
   
-    const { draw, ...rest } = props
-    const ref = useRef()
-    
-    useEffect(() => {
-      
-      const canvas = ref.current
-      const context = canvas.getContext('2d')
-      let count = 0
-      let animationId
-      
-      const renderer = () => {
-        count++
-        draw(context, count)
-        animationId = window.requestAnimationFrame(renderer)
-      }
-      renderer()
-      
-      return () => {
-        window.cancelAnimationFrame(animationId)
-      }
-    }, [draw])
+  const { draw, ...rest } = props
+  const ref = useCanvas(draw)
   
-  return <canvas ref={ref} {...rest} />
+  return <canvas ref={ref} {...rest}/>
 }
 
 export default Canvas
