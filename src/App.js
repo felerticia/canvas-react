dfd6470fa36421ea69a31a05b3f45b6b07ca4e36
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-03-06 11:19:08 +0100

    Animation and outsourcing the draw func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import Canvas from "./Canvas/Canvas";
 
 function App() {
-  return <Canvas width="800" height="800" style={{border:'1px solid black'}}/>
+  const draw = (context,count) => {
+
+
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+    context.fillStyle = 'grey'
+    const d = count % 800
+    context.fillRect(10 +d , 10  , 100 , 100)
+
+  }
+
+
+  return <Canvas draw={draw} width="800" height="800" style={{border:'1px solid black'}}/>
 }
 
 export default App;
--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -3,35 +3,29 @@ import React, { useRef, useEffect } from 'react'
 
 const Canvas = props => {
   
-  const ref = useRef()
-  
-  const draw = (context, count) => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-    context.fillStyle = 'grey'
-    const d = count % 800
-    context.fillRect(10 +d , 10  , 100 , 100)
-  }
-  
-  useEffect(() => {
+    const { draw, ...rest } = props
+    const ref = useRef()
     
-    const canvas = ref.current
-    const context = canvas.getContext('2d')
-    let count = 0
-    let animationID
-    
-    const renderer = () => {
-      count++
-      draw(context, count)
-      animationID = window.requestAnimationFrame(renderer)
-    }
-    renderer()
-    
-    return () => {
-      window.cancelAnimationFrame(animationID)
-    }
-  }, [draw])
+    useEffect(() => {
+      
+      const canvas = ref.current
+      const context = canvas.getContext('2d')
+      let count = 0
+      let animationId
+      
+      const renderer = () => {
+        count++
+        draw(context, count)
+        animationId = window.requestAnimationFrame(renderer)
+      }
+      renderer()
+      
+      return () => {
+        window.cancelAnimationFrame(animationId)
+      }
+    }, [draw])
   
-  return <canvas ref={ref} {...props}/>
+  return <canvas ref={ref} {...rest} />
 }
 
 export default Canvas
