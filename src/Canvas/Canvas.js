50d06a9188f2866fc6dad4b2061a8104f81257ec
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-03-06 10:51:22 +0100

    Animation

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -1,15 +1,35 @@
-import { useRef, useEffect } from 'react'
+
+import React, { useRef, useEffect } from 'react'
 
 const Canvas = props => {
-
-    const ref = useRef()
-
-    useEffect(() => {
-        const canvas = ref.current
-        const context = canvas.getContext('2d')
-        context.fillStyle = 'grey'
-        context.fillRect(10, 10, context.canvas.width - 20, context.canvas.height - 20)
-    }, [])
+  
+  const ref = useRef()
+  
+  const draw = (context, count) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+    context.fillStyle = 'grey'
+    const d = count % 800
+    context.fillRect(10 +d , 10  , 100 , 100)
+  }
+  
+  useEffect(() => {
+    
+    const canvas = ref.current
+    const context = canvas.getContext('2d')
+    let count = 0
+    let animationID
+    
+    const renderer = () => {
+      count++
+      draw(context, count)
+      animationID = window.requestAnimationFrame(renderer)
+    }
+    renderer()
+    
+    return () => {
+      window.cancelAnimationFrame(animationID)
+    }
+  }, [draw])
   
   return <canvas ref={ref} {...props}/>
 }
