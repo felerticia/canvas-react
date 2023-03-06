2f6049759a59c375ee7600c8549c15d88d9ddaef
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-03-06 10:37:42 +0100

    Step 2 : Ref / Context / Draw

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Canvas from "./Canvas/Canvas";
 
 function App() {
-  return <Canvas width="800" height="800"/>
+  return <Canvas width="800" height="800" style={{border:'1px solid black'}}/>
 }
 
 export default App;
--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -1,3 +1,17 @@
-const Canvas = props => <canvas {...props}/>
+import { useRef, useEffect } from 'react'
+
+const Canvas = props => {
+
+    const ref = useRef()
+
+    useEffect(() => {
+        const canvas = ref.current
+        const context = canvas.getContext('2d')
+        context.fillStyle = 'grey'
+        context.fillRect(10, 10, context.canvas.width - 20, context.canvas.height - 20)
+    }, [])
+  
+  return <canvas ref={ref} {...props}/>
+}
 
 export default Canvas
